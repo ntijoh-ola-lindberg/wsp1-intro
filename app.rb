80625914d88710733b3d62a56e5ba089aaa7c2a6
Ola Lindberg <ola.lindberg@ntig.se>
2024-08-22 15:20:57 +0200

    Rövarspråket

--- app.rb
+++ app.rb
@@ -31,16 +31,14 @@ class App < Sinatra::Base
         end
     end
 
-    get '/word_to_num/:word' do | word |
-        @num = nil
-        case word.downcase
-        when "one"
-            @num = 1
-        when "two"
-            @num = 2
-        end
-      
-        erb :words
+    get '/num_to_month/:num' do | num |
+        months = {
+            1 => "Jan",
+            2 => "Feb",
+            3 => "March"
+        }
+
+        months[num.to_i]
     end
 
     get '/add/:first/:second' do | first, second |
@@ -48,4 +46,29 @@ class App < Sinatra::Base
 
         erb :math
     end
+
+    get '/rovarorda/:word' do | word |        
+        @chars = word.chars
+
+        konsonanter = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v","w", "x", "z"]
+
+        @rovarord = ""
+
+        @chars.each do |c| 
+            if konsonanter.include?(c)
+                @rovarord += + c + "o" + c
+            else 
+                @rovarord += + c
+            end
+        end
+    
+        erb :rovarspraka
+    end
+
+    #1 Visa alla 12 månader på en lista på en ny ERB-sida
+    #2 Visa en slumpad månad varje gång du laddar om sidan
+    #3 Visa hur många månader det, idag, är kvar på året 
+    #4 Lägg till fler räknesätt och / eller utöka räknaren på andra sätt
+    #
+    #5 Bygg en rövarspråks-metod https://barnensbibliotek.se/artiklar/lar-dig-rovarspraket
 end
